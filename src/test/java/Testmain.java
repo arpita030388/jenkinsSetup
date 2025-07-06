b0766a4c010a80f84051e09aaad6ddaa010c31fa
bhowmikarpita <bhowmikarpita1222@gmail.com>
2025-07-06 19:46:18 +0530

    Test Classes

---
+++ src/test/java/Testmain.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+import static org.example.Main.printNumber;
+
+public class Testmain {
+
+    @Test
+    public void TestPrintNumber(){
+        printNumber();
+    }
+
+}
